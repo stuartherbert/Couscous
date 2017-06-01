90e3062cb3d971c0eabd9dba791d617127908291
Stuart Herbert <stuart@stuartherbert.com>
2017-06-01 20:06:04 +0100

    New: render Twig before Markdown

--- src/Module/Markdown/Step/RenderMarkdown.php
+++ src/Module/Markdown/Step/RenderMarkdown.php
@@ -8,6 +8,11 @@ use Couscous\Module\Template\Model\HtmlFile;
 use Couscous\Step;
 use Mni\FrontYAML\Parser;
 
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+use Twig_Environment;
+use Twig_Loader_Filesystem;
+
 /**
  * Turns Markdown to HTML.
  *
@@ -31,25 +36,49 @@ class RenderMarkdown implements Step
         $markdownFiles = $project->findFilesByType('Couscous\Module\Markdown\Model\MarkdownFile');
 
         foreach ($markdownFiles as $markdownFile) {
-            $htmlFile = $this->renderFile($markdownFile);
+            $htmlFile = $this->renderFile($project, $markdownFile);
 
             $project->replaceFile($markdownFile, $htmlFile);
         }
     }
 
-    private function renderFile(MarkdownFile $file)
+    private function renderFile(Project $project, MarkdownFile $file)
     {
-        $document = $this->markdownParser->parse($file->getContent());
+        $twig = $this->createTwig(
+            $project->metadata['template.directory'],
+            $project->includedDirectories()
+        );
+        $context = array_merge(
+            $project->metadata->toArray(),
+            $file->getMetadata()->toArray()
+        );
 
-        $filename = $this->replaceExtension($file->relativeFilename);
+        $mdFilename = $this->replaceExtension($file->relativeFilename);
+        $content = $twig->render($mdFilename, $context);
+        if ($mdFilename == 'classes-objects/get_class_properties.md') {
+            echo PHP_EOL . $content . PHP_EOL . PHP_EOL;
+        }
+        $document = $this->markdownParser->parse($content);
 
-        return new HtmlFile($filename, $document->getContent(), $file);
+        return new HtmlFile($file->relativeFilename, $document->getContent(), $file);
     }
 
     private function replaceExtension($filename)
     {
         $filename = substr($filename, 0, strrpos($filename, '.'));
 
-        return $filename.'.html';
+        return $filename.'.md';
+    }
+
+    private function createTwig($templateDirectory, $sourceDirectories)
+    {
+        $loadDirs = array_merge([$templateDirectory], $sourceDirectories);
+        $loader = new Twig_Loader_Filesystem($loadDirs);
+
+        return new Twig_Environment($loader, [
+            'cache'       => false,
+            'auto_reload' => true,
+            'autoescape' => false,
+        ]);
     }
 }
